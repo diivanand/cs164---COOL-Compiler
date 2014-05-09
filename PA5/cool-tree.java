414dfbd662319d9983842c5353aed1e22c726a76
minyoon0205 <minyoon0205@gmail.com>
2014-05-08 18:42:57 -0700

    still working on DISPATCH. please read the TODO update

--- PA5/cool-tree.java
+++ PA5/cool-tree.java
@@ -304,6 +304,8 @@ class class_c extends Class_ {
     protected AbstractSymbol parent;
     protected Features features;
     protected AbstractSymbol filename;
+
+    // protected static class_c lastClass;
     /** Creates "class_c" AST node. 
      *
      * @param lineNumber the line in the source file from which this node came.
@@ -563,11 +565,20 @@ class assign extends Expression {
      * */
     public void code(PrintStream s, CgenClassTable cgenTable) {
         CgenSupport.emitComment(s, "Entered cgen for assign");
-        expr.code(s, cgenTable);
-        //the offset in the object that this attribute is located in
-        int offset = 9999;
-        //Store object in appropriate spot in frame pointer
-        CgenSupport.emitStore(CgenSupport.ACC, offset, CgenSupport.FP, s);
+        // Cool Manual 7.2
+        // "Identifier self may be referenced, but it is an error to
+        // assign to self or to bind self in a let, a case, or
+        // as a formal parameter. It is also illegal to have attributes
+        // named self"
+        if(name.equals(TreeConstants.self)) {
+            //CgenSupport.emitMove(CgenSupport.ACC, CgenSupport.SELF, s);
+        } else {
+            expr.code(s, cgenTable);
+            //the offset in the object that this attribute is located in
+            int offset = 9999;
+            //Store object in appropriate spot in frame pointer
+            CgenSupport.emitStore(CgenSupport.ACC, offset, CgenSupport.FP, s);
+        }
         CgenSupport.emitComment(s, "Leaving cgen for assign");
     }
 
@@ -684,25 +695,44 @@ class dispatch extends Expression {
      * @param s the output stream 
      * */
     public void code(PrintStream s, CgenClassTable cgenTable) {
+        CgenSupport.emitComment(s, "dispatch code START");
         AbstractSymbol exprType = expr.get_type();
         if (exprType.equals(TreeConstants.SELF_TYPE)) {
             // assign the current type to exprType 
+            //exprType = class_c.lastClass.name;
+            exprType = CgenNode.getLatestType();
+            System.out.println("SELF_TYPE converted to "+exprType);
         }
-        CgenNode c1 = (CgenNode) programc.codegen_classtable.probe(exprType);
+        //CgenNode c1 = (CgenNode) programc.codegen_classtable.probe(exprType);
+        CgenNode c1 = (CgenNode) cgenTable.probe(exprType);
         
-        this.pushArgs(actual.getElements(), cgenTable, s);
-    }
-    /**
-     * Helper method that recursively code arguments in order.
-     */
-    private void pushArgs(Enumeration en, CgenClassTable cct, PrintStream s) {
-        Expression argExpr = (Expression) en.nextElement();
-        if(en.hasMoreElements()) {
-            pushArgs(en,cct,s);
+        //this.pushArgs(actual.getElements(), cgenTable, s);
+        for(Enumeration en = actual.getElements(); en.hasMoreElements(); ) {
+            ((Expression) en.nextElement()).code(s, cgenTable);
+            CgenSupport.emitPush(CgenSupport.ACC,s);
         }
-        argExpr.code(s, cct);
-        CgenSupport.emitPush(CgenSupport.ACC, s);
+
+        expr.code(s, cgenTable);
+        CgenSupport.emitPartialLoadAddress(CgenSupport.T1, s);
+        CgenSupport.emitDispTableRef(c1.name, s);
+        s.println();
+        System.out.println(c1);
+        CgenSupport.emitLoad(CgenSupport.T1, c1.getMethodIndex(name), CgenSupport.T1, s);
+        CgenSupport.emitJalr(CgenSupport.T1, s);
+        CgenSupport.emitMove(CgenSupport.ACC, CgenSupport.SELF, s);
+        CgenSupport.emitComment(s, "dispatch code done");
     }
+    ///**
+    // * Helper method that recursively code arguments in order.
+    // */
+    //private void pushArgs(Enumeration en, CgenClassTable cct, PrintStream s) {
+    //    if(en.hasMoreElements()) {
+    //        Expression argExpr = (Expression) en.nextElement();
+    //        pushArgs(en,cct,s);
+    //        argExpr.code(s, cct);
+    //        CgenSupport.emitPush(CgenSupport.ACC, s);
+    //    }
+    //}
 
 
 }
