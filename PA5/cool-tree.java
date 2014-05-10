3696d8b8a25e613e96a3b5758ab7c7f717ee0612
minyoon0205 <minyoon0205@gmail.com>
2014-05-10 01:59:15 -0700

    passed tests/3.cl

--- PA5/cool-tree.java
+++ PA5/cool-tree.java
@@ -1531,7 +1531,7 @@ class lt extends Expression {
         //Move it to a temp
         CgenSupport.emitMove(CgenSupport.T1, CgenSupport.ACC, s);
         //Generate Code for second expression
-        e1.code(s, cgenTable);
+        e2.code(s, cgenTable);
 
         //check if $t1 < $a0 is true or false
         int labelCountTrue = CgenNode.getLabelCountAndIncrement();
@@ -1663,7 +1663,7 @@ class leq extends Expression {
         //Move it to a temp
         CgenSupport.emitMove(CgenSupport.T1, CgenSupport.ACC, s);
         //Generate Code for second expression
-        e1.code(s, cgenTable);
+        e2.code(s, cgenTable);
 
         //check if $t1 < $a0 is true or false
         int labelCountTrue = CgenNode.getLabelCountAndIncrement();
